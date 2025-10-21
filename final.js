c0b658d723493d28d5f8ba8c56dcaf34267a2522
MikuuZ <larumel166@gmail.com>
2025-10-21 15:53:08 -0300

    Primer commit

--- final.js
+++ final.js
@@ -1,15 +1,1 @@
-const prompt = require('prompt-sync')();
-
-console.log("Bienvenido a la tienda Lilo");
-
-const prenda1 = {
-    "nombre": "pantalon",
-    "tipo" : "jean",
-    "marca" : "venus",
-    "precio" : $35000
-
-
-}
-
-
 
